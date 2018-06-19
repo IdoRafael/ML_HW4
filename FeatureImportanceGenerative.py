a5336b7620325ea31f9a5cf360ad4ead922325ce
Ran Lottem <ran.lottem@campus.technion.ac.il>
2018-06-19 15:44:11 +0300

    Correct Figures

--- FeatureImportanceGenerative.py
+++ FeatureImportanceGenerative.py
@@ -7,13 +7,28 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import GridSearchCV, KFold
 
 
 def get_LDA_classifier(X_train, y_train, X_test, y_test):
     # TODO: which solver?
-    lda = LinearDiscriminantAnalysis(solver='eigen')
+    n_splits = 5
+    # model = GridSearchCV(LinearDiscriminantAnalysis(), param_grid=[{'n_components': list(range(3,7))}], cv=5)
+    # model.fit(X_train, y_train)
+    # print('score for optimal is {}'.format(model.score(X_test, y_test)))
+    # kf = KFold(n_splits=n_splits)
+    # score_total = 0
+    # for train_index, val_index in kf.split(X_train):
+    #     lda = LinearDiscriminantAnalysis(solver='eigen')
+    #     X_train_fold, X_val_fold = np.array(X_train)[train_index], np.array(X_train)[val_index]
+    #     y_train_fold, y_val_fold = np.array(y_train)[train_index], np.array(y_train)[val_index]
+    #     score = lda.fit(X_train_fold, y_train_fold).score(X_val_fold, y_val_fold)
+    #     score_total += score
+    # avg_score = score_total/n_splits
+    # print("average accuracy for LDA using {}-fold CV is {:.2f}".format(n_splits, avg_score))
     # TODO: should this be done using CV?
     # X_train_new = lda.fit_transform(X_train, y_train)
+    lda = LinearDiscriminantAnalysis(solver='eigen')
     lda.fit(X_train, y_train)
     score = lda.score(X_test, y_test)
     print('score for LDA classifier is {}'.format(score))
@@ -23,6 +38,7 @@ def get_LDA_classifier(X_train, y_train, X_test, y_test):
 def get_LDA_coefficients(lda):
     coefficients = lda.coef_
     max_args = np.argmax(coefficients, axis=1)
+    # print(coefficients)
     max_args_abs = np.argmax(np.abs(coefficients), axis=1)
     return max_args, max_args_abs, coefficients
 
@@ -37,7 +53,7 @@ def plot_leading_features(max_args, max_args_abs, coefficients, df):
                       attribute_index_to_name[max_arg_index],
                       coefficients[i][max_arg_index]))
 
-        plt.figure(figsize=(8,6))
+        plt.figure(figsize=(8, 6))
         plt.subplots_adjust(left=0.4)
         plt.title('{} most important factors\n(larger (abs) is better)'.format(code_to_name[i]))
         plt.barh(X_test.columns.values, coefficients[i])
@@ -45,6 +61,17 @@ def plot_leading_features(max_args, max_args_abs, coefficients, df):
 
         # commented out saving since we have the graphics
         # plt.savefig('{}_LDA.png'.format(code_to_name[i]))
+
+        plt.show()
+        plt.close()
+
+        plt.figure(figsize=(8, 6))
+        plt.subplots_adjust(left=0.4)
+        plt.title('{} most important factors %'.format(code_to_name[i]))
+        plt.barh(X_test.columns.values, 100 * (np.abs(coefficients[i]) / np.sum(np.abs(coefficients[i]))))
+
+        # commented out saving since we have the graphics
+        # plt.savefig('{}_LDA_percent.png'.format(code_to_name[i]))
 
         plt.show()
         plt.close()
